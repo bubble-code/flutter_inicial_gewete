f050c636a7e8f284cb2f01970abe9b85d4155609
Alejandro Obregon <aobregonrodri@gmail.com>
2022-05-18 23:17:54 +0200

    trabajando en Gewete page

--- lib/ui/widget/ButtomGeweteGrid.dart
+++ lib/ui/widget/ButtomGeweteGrid.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:money2/money2.dart';
+import 'package:decimal/decimal.dart';
 
 class ButtonGeweteGrid extends StatefulWidget {
   final String title;
@@ -53,7 +56,9 @@ class _ButtonGeweteGridState extends State<ButtonGeweteGrid> {
   @override
   Widget build(BuildContext context) {
     List<Widget> result = [];
+    double total = 0.0;
     widget.valores.forEach((key, value) {
+      total += value;
       result.add(
         Container(
           padding: const EdgeInsets.only(
@@ -83,21 +88,59 @@ class _ButtonGeweteGridState extends State<ButtonGeweteGrid> {
         //  ConnetionDB cc = ConnetionDB(widget.ip, widget.passw);
       },
       child: Container(
-        margin: const EdgeInsets.all(15),
-        padding: const EdgeInsets.all(5),
-        height: 130,
+        margin: const EdgeInsets.only(top: 10, left: 14, right: 14),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        // height: 130,
         decoration: BoxDecoration(
-          color: widget.color,
-          borderRadius: BorderRadius.circular(5),
+          // color: widget.color,
+          borderRadius: BorderRadius.circular(7),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              widget.title,
-              style: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.bold),
+            Container(
+              width: MediaQuery.of(context).size.width * 2 / 3,
+              margin: const EdgeInsets.only(left: 8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.title,
+                    style: GoogleFonts.rajdhani(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue[50],
+                        borderRadius: BorderRadius.circular(7)),
+                    child: Text(
+                      Money.fromDecimal(Decimal.parse(total.toString()),
+                              code: 'EUR')
+                          .toString(),
+                      // " € " + total.toStringAsFixed(2) + " ",
+                      style: GoogleFonts.rajdhani(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 1,
+              alignment: Alignment.centerLeft,
+              child: Divider(
+                color: Colors.amber[700],
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
